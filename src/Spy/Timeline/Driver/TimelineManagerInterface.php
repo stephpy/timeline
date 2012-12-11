0658ad802293c7734b1c67cde7a57ddad3a1b7ce
Stéphane PY <py.stephane1@gmail.com>
2012-12-11 19:03:36 +0100

    Add driver API

---
+++ src/Spy/Timeline/Driver/TimelineManagerInterface.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Spy\Timeline\Driver;
+
+use Spy\Timeline\Model\ActionInterface;
+use Spy\Timeline\Model\ComponentInterface;
+use Spy\Timeline\Model\TimelineInterface;
+
+/**
+ * TimelineManagerInterface
+ *
+ * @author Stephane PY <py.stephane1@gmail.com>
+ */
+interface TimelineManagerInterface
+{
+    /**
+     * @param ComponentInterface $subject subject
+     * @param array              $options options
+     */
+    public function getTimeline(ComponentInterface $subject, array $options = array());
+
+    /**
+     * count how many keys are stored
+     *
+     * @param ComponentInterface $subject subject
+     * @param array              $options options
+     *
+     * @return integer
+     */
+    public function countKeys(ComponentInterface $subject, array $options = array());
+
+    /**
+     * remove key from storage
+     * This action has to be flushed
+     *
+     * @param  ComponentInterface $subject  subject
+     * @param  string             $actionId The action id
+     * @param  array              $options  Array of options
+     * @return void
+     */
+    public function remove(ComponentInterface $subject, $actionId, array $options = array());
+
+    /**
+     * remove all keys from storage
+     * This action has to be flushed
+     *
+     * @param  ComponentInterface $subject  subject
+     * @param  array  $options      Array of options
+     * @return void
+     */
+    public function removeAll(ComponentInterface $subject, array $options = array());
+
+    /**
+     * create and persist a timeline.
+     *
+     * @param ActionInterface    $action  action
+     * @param ComponentInterface $subject subject
+     * @param string             $context context
+     * @param mixed              $type    timeline type (timeline, notification)
+     */
+    public function createAndPersist(ActionInterface $action, ComponentInterface $subject, $context = 'GLOBAL', $type = TimelineInterface::TYPE_TIMELINE);
+
+    /**
+     * flush persist timelines
+     */
+    public function flush();
+}
