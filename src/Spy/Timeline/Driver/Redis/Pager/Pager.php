0dc88989c87392c729e40d27b1b38f7bd8484448
Stéphane PY <py.stephane1@gmail.com>
2014-05-30 09:29:59 +0200

    Redis pager implements \ArrayAccess.

--- src/Spy/Timeline/Driver/Redis/Pager/Pager.php
+++ src/Spy/Timeline/Driver/Redis/Pager/Pager.php
@@ -10,9 +10,10 @@ use Spy\Timeline\ResultBuilder\Pager\PagerInterface;
  *
  * @uses AbstractPager
  * @uses PagerInterface
+ * @uses ArrayAccess
  * @author Stephane PY <py.stephane1@gmail.com>
  */
-class Pager extends AbstractPager implements PagerInterface, \IteratorAggregate, \Countable
+class Pager extends AbstractPager implements PagerInterface, \IteratorAggregate, \Countable, \ArrayAccess
 {
     /**
      * @var integer
@@ -95,5 +96,44 @@ class Pager extends AbstractPager implements PagerInterface, \IteratorAggregate,
     public function count()
     {
         return count($this->items);
+    }
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$offset] = $value;
+        }
+    }
+
+    /**
+     * @param  mixed   $offset
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
+    }
+
+    /**
+     * @param  mixed $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->items[$offset]) ? $this->items[$offset] : null;
     }
 }
